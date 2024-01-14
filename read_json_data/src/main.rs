0333003bed4f0eafd8b2977ae39e9bc0cb5085ee
eliashezron <eliashezron23@gmail.com>
2024-01-14 23:41:47 +0300

    read_json_file

--- read_json_data/src/main.rs
+++ read_json_data/src/main.rs
@@ -1,25 +1,21 @@
-extern crate serde;
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Serialize, Deserialize)]
-struct Paragraph {
+struct Paragraph { //create a struct for the paragraph
     name: String
 }
-
-
 #[derive(Serialize, Deserialize)]
-struct Article {
+struct Article { //create a struct for the article
     article: String,
     author: String,
     paragraph: Vec<Paragraph>
 }
 
-fn main() {
-    let json = r#"
+fn main() { //create a json string
+    let json = r#" 
 {
   "article": "how to work with json in Rust",
-  "author": "akhil",
+  "author": "elias",
   "paragraph": [
     {
       "name": "starting sentences"
@@ -33,12 +29,10 @@ fn main() {
   ]
 }
 "#;
-    let parsed: Article = read_json_typed(json);
+    let parsed: Article = read_json_typed(json); //parse the json string
 
-    println!("\n\n The name of the first paragraph is: {}", parsed.paragraph[0].name);
+    println!("\n\n The name of the first paragraph is: {}", parsed.paragraph[2].name); //print the name of the first paragraph
 }
-
-
 fn read_json_typed(raw_json: &str) -> Article {
     let parsed: Article = serde_json::from_str(raw_json).unwrap();
     return parsed
