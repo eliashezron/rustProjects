568f74864ec9366855a2db5260fbcf64c8a64af1
eliashezron <eliashezron23@gmail.com>
2024-01-21 17:17:27 +0300

    async_wait

--- async_await/src/main.rs
+++ async_await/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+
+error_chain!{
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+#[tokio::main]
+async fn main() -> Result<()> {
+    let response = reqwest::get("https://httpbin.org/get").await?; //get the response from the url
+    println!("Status: {}", response.status()); //print the status
+    println!("Headers:\n{:#?}", response.headers()); //print the headers
+
+    let body = response.text().await?; //get the body
+    println!("Body:\n{}", body); //print the body
+    Ok(())
 }
